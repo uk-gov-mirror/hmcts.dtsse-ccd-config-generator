559f7940af5f870030428422157a78ef1be3a059
sjamihmcts <santoshini.jami@hmcts.net>
2022-09-28 11:34:15 +0100

    Added complexType to LinkReason

--- ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/LinkReason.java
+++ ccd-config-generator/src/main/java/uk/gov/hmcts/ccd/sdk/type/LinkReason.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.ccd.sdk.type;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 @Getter
 @RequiredArgsConstructor
+@ComplexType(name = "LinkReason", generate = false)
 // TODO: Revisit this enum as this data needs to taken from Reference data
 public enum LinkReason {
     REASON_1("Reason 1", "Reason desc");
